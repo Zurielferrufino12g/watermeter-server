199f200530a4f7ae642a053fdc205565778411b7
Zuriel Velasquez <zurielv87@gmail.com>
2026-02-01 22:16:47 -0400

    Process Backend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,6 +7,6 @@ export default defineConfig({
   publicDir: "public",
   build: {
     outDir: "dist",
-    emptyOutDir: true, // ✅ deja esto en true
+    emptyOutDir: true, // ✅ deja esto en tru
   },
 });
